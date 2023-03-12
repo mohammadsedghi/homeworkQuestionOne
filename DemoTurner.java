3eb9302440f44f3109b36ed4b5b432a4739eeeb8
mohammad <mohammadsedghi1993@gmail.com>
2023-03-12 19:14:19 +0330

    create object from Leaf,Page,Pancake and run thier method

--- DemoTurner.java
+++ DemoTurner.java
@@ -2,6 +2,11 @@ package questionone;
 
 public class DemoTurner {
     public static void main(String[] args) {
-        
+        Leaf leaf=new Leaf();
+        Page page=new Page();
+        Pancake pancake=new Pancake();
+        leaf.turn();
+        page.turn();
+        pancake.turn();
     }
 }
