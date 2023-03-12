df4037ce04d921fc4dc3ffc862b6ab442ccb9bdd
mohammad <mohammadsedghi1993@gmail.com>
2023-03-12 19:38:17 +0330

    create object from Lamp and Driver class in DemoTurner

--- DemoTurner.java
+++ DemoTurner.java
@@ -5,8 +5,12 @@ public class DemoTurner {
         Leaf leaf=new Leaf();
         Page page=new Page();
         Pancake pancake=new Pancake();
+        Lamp lamp=new Lamp();
+        Driver driver=new Driver();
         leaf.turn();
         page.turn();
         pancake.turn();
+        lamp.turn();
+        driver.turn();
     }
 }
