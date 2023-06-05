588065f234ac44c83ca59ad527fc8b707781e99d
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-06-05 20:25:31 +0330

    new chnages

--- DemoTurner2.java
+++ DemoTurner2.java
@@ -3,6 +3,7 @@ package questionone;
 public class DemoTurner2 {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println();
         Leaf leaf=new Leaf();
         Page page=new Page();
         Pancake pancake=new Pancake();
